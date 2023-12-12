25e6e701a3faab31bf51b1e18f41786418c7940e
ABHISHEK KHATRI <60095100+fgcsl@users.noreply.github.com>
2023-12-12 17:47:46 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS <database_name>;
 USE <database_name>;
 
 -- Create the second table file information (path of the file)
-CREATE TABLE IF NOT EXISTS test2_file_info (
+CREATE TABLE IF NOT EXISTS tagger_file_info (
     file_path_id INT PRIMARY KEY AUTO_INCREMENT,
     file_name VARCHAR(255) NOT NULL,
     system_path TEXT NOT NULL,
@@ -15,17 +15,17 @@ CREATE TABLE IF NOT EXISTS test2_file_info (
 );
 
 -- Create the third table with a foreign key reference file_name and file_parent_node
-CREATE TABLE IF NOT EXISTS test2_f_n (
+CREATE TABLE IF NOT EXISTS tagger_file_node (
     file_path_id INT,
     file_n VARCHAR(255) NOT NULL,
     fp_node VARCHAR(80) NOT NULL,
     UNIQUE(file_n, fp_node),
-    FOREIGN KEY (file_path_id) REFERENCES test2_file_info(file_path_id)
+    FOREIGN KEY (file_path_id) REFERENCES tagger_file_info(file_path_id)
 );
 
 
 -- Create the first table node node relationship
-CREATE TABLE IF NOT EXISTS test2_n_n (
+CREATE TABLE IF NOT EXISTS tagger_node_node (
     id INT AUTO_INCREMENT PRIMARY KEY,
     c_node VARCHAR(80) NOT NULL,
     p_node VARCHAR(80) NOT NULL,
